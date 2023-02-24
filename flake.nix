9eb2832878db65d39227f322fcd172c6ce55f2f8
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2023-02-24 14:08:06 -0800

    Add a shell for esp-idf development with Rust

--- flake.nix
+++ flake.nix
@@ -35,6 +35,7 @@
         esp32-idf = import ./shells/esp32-idf.nix { inherit pkgs; };
         esp32c3-idf = import ./shells/esp32c3-idf.nix { inherit pkgs; };
         esp32s2-idf = import ./shells/esp32s2-idf.nix { inherit pkgs; };
+        esp32s2-idf-rust = import ./shells/esp32s2-idf-rust.nix { inherit pkgs; };
         esp32s3-idf = import ./shells/esp32s3-idf.nix { inherit pkgs; };
         esp32c6-idf = import ./shells/esp32c6-idf.nix { inherit pkgs; };
         esp32h2-idf = import ./shells/esp32h2-idf.nix { inherit pkgs; };
